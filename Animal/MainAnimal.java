f52b429ad79f6ff0a7b3074beae566367ddad5b8
JuanCarlosCarbajosaGarcia <juanc.cargar@educa.jcyl.es>
2025-11-12 10:28:35 +0100

    v0.4

--- Animal/MainAnimal.java
+++
@@ -1,7 +0,0 @@
-package Animal;
-
-public class MainPolimorfismo {
-    public static void main(String[] args) {
-        Animal p = new perro()
-    }
-}
--- Vehiculo/MainVehiculo.java
+++
@@ -1,7 +0,0 @@
-package Vehiculo;
-
-public class MainVehiculo {
-    public static void main(String[] args) {
-        Coche coche = new Coche();
-    }
-}
